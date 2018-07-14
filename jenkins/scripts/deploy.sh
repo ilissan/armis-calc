03d59094c997ef0386ddb674cf74d320ef48325f
admin <admin@omnius.com>
2018-07-15 01:34:55 +0300

    add result validation in deploy step

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -23,10 +23,25 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar target/${NAME}-${VERSION}.jar <<- EOF
+expected_result='(i=37,j=1,x=6,y=35)'
+IFS=$','
+read -a sorted_arr_exp_rslt <<< $(echo $expected_result | sed 's/[)(]//g')
+# get actual result
+actual_result=$(java -jar target/${NAME}-${VERSION}.jar <<- EOF
 i=0
 j=++i
 x=i+++5
 y=5+3*10
 i+=y
-EOF
+EOF 2>&1 )
+#convert result into arr and sort
+read -a arr_actual_result <<< $(echo $actual_result | sed 's/[)(]//g')
+sorted_arr_act_rslt=($(sort <<<"${arr_actual_result[*]}"))
+unset IFS
+# compare results
+[ ${#sorted_arr_exp_rslt[*]} != ${#sorted_arr_act_rslt[*]} ] && { echo arrays different size; exit 1; }
+for ii in ${!sorted_arr_exp_rslt[*]}; do
+     [ "${sorted_arr_exp_rslt[$ii]}" == "${sorted_arr_act_rslt[$ii]}" ] || { echo different element $ii; exit 1; }
+done
+echo arrays identical
+exit 0
