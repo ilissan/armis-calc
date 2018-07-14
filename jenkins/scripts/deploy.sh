5cc060c92170d516de218f4ec88733627d00cc2c
admin <admin@omnius.com>
2018-07-14 21:26:54 +0300

    add test expression

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -23,4 +23,10 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar target/${NAME}-${VERSION}.jar 'i=0 j=++i x=i++ +5 y=5+3*10 i+=y'
+java -jar target/${NAME}-${VERSION}.jar 
+i=0
+j=++i
+x=i++
++5
+y=5+3*10
+i+=y
