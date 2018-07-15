74b536acac852a1862fe2e3365eecdb97aa43bee
admin <admin@omnius.com>
2018-07-15 23:39:53 +0300

    Fix result output

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -27,15 +27,17 @@ expected_result='(i=37,j=1,x=6,y=35)'
 IFS=$','
 read -a sorted_arr_exp_rslt <<< "$(echo $expected_result | sed 's/[()]//g')"
 # get actual result
-actual_result="`java -jar target/${NAME}-${VERSION}.jar <<-EOF
+tmp_actual_result=$(java -jar target/${NAME}-${VERSION}.jar <<-EOF
 i=0
 j=++i
 x=i+++5
 y=5+3*10
 i+=y
 EOF
-`"
-#convert result into arr and sort
+)
+# remove redundant strings from output
+actual_result=$(echo $tmp_actual_result | sed 's/.*[[:space:]]//')
+# convert result into arr and sort
 read -a arr_actual_result <<< "$(echo $actual_result | sed 's/[()]//g')"
 sorted_arr_act_rslt=("$(sort <<<"${arr_actual_result[*]}")")
 unset IFS
